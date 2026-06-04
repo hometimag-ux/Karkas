408ac81c568dda40e436f1f3d2433ebd054a504c
Timur <hometimag@gmail.com>
2026-06-04 08:06:16 +0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const products = [
+  { id: 1, name: "Халат Aqua", price: 3490, wholesalePrice: 2610, emoji: "👩‍⚕️💙" },
+  // ... остальные товары
+];
+
+let cart = [];
+let userRole = "retail";
+let searchQuery = "";
+
+// ... и весь остальной JavaScript код
