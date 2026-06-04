7adf96a462125b8d04838854ae5f8b8b6684a91a
Timur <hometimag@gmail.com>
2026-06-04 15:59:32 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -463,8 +463,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Делаем функции глобальными
 window.closeQuickView = closeQuickView;
-    const modal = document.getElementById('quickViewModal');
-    if (modal) modal.remove();
-    document.body.style.overflow = '';
-}
 window.addToCartById = addToCartById;
